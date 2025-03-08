eda02fb1d5ebcb7af5e027be7d37a5d71b3c496c
Habert Kungu <habert231@gmail.com>
2025-03-08 03:42:37 +0300

    fixing npx bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.html"], // Scans root HTML files
+  content: ["./**/*.html"], // Scans all HTML files in all directories
   theme: {
     extend: {
       colors: {
