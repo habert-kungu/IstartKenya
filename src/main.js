31bb1fa22a1360978689bf42928b1fefd877fcc2
Habert Kungu <habert231@gmail.com>
2025-03-10 09:09:59 +0300

    Add navbar componentto the public folder

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // Load navbar into #navbar
-fetch("../src/components/navbar.html")
+fetch("/components/navbar.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("navbar").innerHTML = data;
   })
   .catch((error) => console.error("Error loading navbar:", error));
-// Load navbar into #navbar
-fetch("../src/components/footer.html")
+// Loadnavbar into #navbar
+fetch("/components/footer.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("footer").innerHTML = data;
